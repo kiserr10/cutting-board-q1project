1e54c7ef08f3d23b6c0cdc53891bcbf5f8a91a13
Ross Kiser <kiserr10@gmail.com>
2017-05-15 13:37:48 -0600

    added roles, wireframes, scroll animations

---
+++ Roles.md
@@ -0,0 +1,10 @@
+# User Roles
+* Chefs
+* Cooks
+* Aspiring Cooks
+* Those who don't cook often
+* Those who don't know how to cook and want to learn through following recipes
+* Foodies
+* Those trying to find healthy diets and start a meal plan
+* People who like browsing food recipes and those who like using technology made simple
+* Average people who want creative ideas for a meal
--- app.js
+++ app.js
@@ -1,48 +1,45 @@
+//***PROGRAM***//
 $(document).ready(function(){
 	$('.parallax').parallax();
 	$('.dropdown-button').dropdown();
 	$('#search-button').click(searchClick);
 	$('.search-form').submit(searchClick);
 
-	// $('#search').keypress(function(ev){
-	// 	if (ev.which === 13) {
-	// 		$('#search-button').click(searchClick);
-	// 	}
-	// });
-
-
 //SLOW SCROLL BACK TO TOP//
-
 	$('.grey-text').click(function () {
 		$('html, body').animate({scrollTop: 0}, 1500);
 	});
 
+//SCROLL DOWN TO BOTTOM FROM TOP//
+	$('.extras-button').click(function () {
+		$('html, body').animate({scrollTop: $('#bottom').height()}, 1500);
 
-
+	});
 });
 
+
+//URL Variables//
 var $searchUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search';  //<----User Input//
 var $instructionsUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/';
 var $imageUrl = 'https://spoonacular.com/recipeImages/';
+//URL Variables//
 
-//***PROGRAM***//
 
-
-//FUNCTION TO APPEND SEARCH RESULT TITLE AND PHOTO//
+//Function to have recipeSearch search for recipes that have keywords from the user string search//
 function searchClick(){
-	event.preventDefault();
-	let $searchString = $('#search').val();
-	$('.card-row').empty();
-	recipeSearch($searchString);
+	event.preventDefault(); //<--Clear defaults for forms/inputs//
+	let $searchString = $('#search').val();  //<--Variable Holding User Input String//
+	$('.card-row').empty();  //<--Every Re-Load Cards Refresh//
+	recipeSearch($searchString);  //<--Calling the function to get recipes by inputting the query of the user input defined above//
 }
 
 
 function searchResult(data) {
 	// console.log(data);
-	let recipes = data.results;
+	let recipes = data.results;  //<--The array of recipe objects//
 
 	for (var i = 0; i < 9; i++) {
-		console.log(recipes[i].id, $imageUrl + recipes[i].image);
+		// console.log(recipes[i].id, $imageUrl + recipes[i].image);
 		$('.card-row').append(
 			`<div class="card medium col s12 m6 l4">
 				<div class="card-image waves-effect waves-block waves-light">
@@ -53,15 +50,18 @@ function searchResult(data) {
 					<p><a href="#">This is a link</a></p>
 				</div>
 				<div class="card-reveal">
-					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-					<p>Here is some more information about this product that is only revealed once clicked on.</p>
+					<span class="card-title grey-text text-darken-4">${recipes[i].title}<i class="material-icons right">close</i></span>
+					<ul class="card-steps${i}"></ul>
+					<h1 class="card-title">Instructions:</h1>
+					<div class="instruction-steps${i}"></div>
+
 				</div>
 			</div>`
 		);
 		ingredientSearch(recipes[i].id);
-
 	}
 }
+
 
 
 // function(data) { console.log(data.results[1]); }
@@ -77,7 +77,7 @@ function recipeSearch(search){
 			'query': search,
 		}, // Additional parameters here
 		dataType: 'json',
-		success: searchResult,
+		success: searchResult,  //<--Callback Function to the API request for
 		error: function(err) { alert(err); },
 		beforeSend: function(xhr) {
 			xhr.setRequestHeader('X-Mashape-Authorization', 'Fj660kzp6mmshIhAIrhx43CKZ4hyp1u1BXTjsnSmeypNvfgMlj'); // Enter here your Mashape key
@@ -87,10 +87,10 @@ function recipeSearch(search){
 
 function ingredientSearch(recipeId){
 	$.ajax({
-		url: $instructionsUrl + recipeId + '/analyzedInstructions', // The URL to the API. You can get this in the API page of the API you intend to consume
+		url: $instructionsUrl + recipeId + '/information', // The URL to the API. You can get this in the API page of the API you intend to consume
 		type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
 		data: {
-			'stepBreakdown':'true',
+			'includeNutrition':'false',
 		}, // Additional parameters here
 		dataType: 'json',
 		success: ingredientResult,
@@ -102,42 +102,26 @@ function ingredientSearch(recipeId){
 }
 
 function ingredientResult(data){
-	console.log(data);
+	console.log(data.instructions);
+	// var $ingredients = [];
+
+	for (var i = 0; i < data.extendedIngredients.length; i++) {
+		$('.card-steps' + i).append(
+			'<li>' + data.extendedIngredients[i].originalString + '</li>'
+		);
+		$('.instruction-steps' + i).append(
+				'<p>' + data.instructions + '</p>'
+			);
+	}
 }
 
 
-
-
-
-
-	// function submitDetailsForm() {
-	// 	$('.search-button').submit();
-	// 	var $recipeRequest = document.getElementById('email').value;
-	// }
-	//
-	// $('#email').submit(function() {
-	//     // get all the inputs into an array.
-	//     var $inputs = $('#email :input');
-	// 		console.log('$inputs')
-	// });
-
-
-
-
-// $('.card-row').append(
-//
-// 			'<div class="col s12 m6 l4">' +
-// 				'<div class="card">' +
-// 					'<div class="card-image">' +
-// 						'<img src="' + response[i].recipe.image + '">' +
-// 						'<span class="card-title">' + recipes[i].title +'</span>'
-// 					</div>
-// 					<div class="card-content">
-// 						<p>I am a very simple card. I am good at containing small bits of information.
-// 						I am convenient because I require little markup to use effectively.</p>
-// 					</div>
-// 					<div class="card-action">
-// 						<a href="#">This is a link</a>
-// 					</div>
-// 				</div>
-// 			</div>');
+// function procedureResult(data){
+// 	console.log(data.instructions);
+// 	// var $ingredients = [];
+// 	for (var i = 0; i < data.length; i++) {
+// 		$('.instruction-steps').append(
+// 			'<p>' + data.instructions + '</p>'
+// 		);
+// 	}
+// }
