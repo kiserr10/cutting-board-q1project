ea92ae221c2e87702f6e93578905a10da944a9ff
Ross Kiser <kiserr10@gmail.com>
2017-05-19 07:44:56 -0600

    deploy ready for presentation

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 //***PROGRAM***//
 $(document).ready(function(){
+	$('#page-load').fadeIn(1500);
+	$('#wrapper').fadeIn(2000);
 	$('.parallax').parallax();
 	$('.dropdown-button').dropdown();
 	$('#search-button').click(searchClick);
 	$('.search-form').submit(searchClick);
+
+	//SIDE NAV//
+	$('.button-collapse').sideNav({
+		menuWidth: 100, // Default is 300
+		edge: 'left', // Choose the horizontal origin
+		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+		draggable: true // Choose whether you can drag to open on touch screens
+	});
 
 //SLOW SCROLL BACK TO TOP//
 	$('.grey-text').click(function () {
@@ -20,12 +30,18 @@ $(document).ready(function(){
 			scrollTop: $('.search').offset().top
 		}, 1500);
 	});
+
 	$('.search-form').submit(function() {
 		$('html, body').stop(true, true).delay(1700).animate({
 			scrollTop: $('.search').offset().top
 		}, 1500);
 	});
 
+	$('.right-arrow').click(function() {
+		$('html, body').stop(true, true).delay(1700).animate({
+			scrollTop: $('.search').offset().top
+		}, 1500);
+	});
 });
 
 
@@ -33,28 +49,36 @@ $(document).ready(function(){
 var $searchUrl = 'https://recipe-search.now.sh/recipes/search';  //<----User Input//
 var $instructionsUrl = 'https://recipe-search.now.sh/recipes/';
 var $imageUrl = 'https://spoonacular.com/recipeImages/';
+var page = 0;
+var perPage = 9;
+var currentSearch;
 //URL Variables//
 
 
 //Function to have recipeSearch search for recipes that have keywords from the user string search//
 function searchClick(){
 	event.preventDefault(); //<--Clear defaults for forms/inputs//
-	let $searchString = $('#search').val();  //<--Variable Holding User Input String//
+	currentSearch = $('#search').val();  //<--Variable Holding User Input String//
 	$('.card-row').empty();  //<--Every Re-Load Cards Refresh//
-	recipeSearch($searchString);  //<--Calling the function to get recipes by inputting the query of the user input defined above//
+	recipeSearch();  //<--Calling the function to get recipes by inputting the query of the user input defined above//
+	buttonDisable();
+	setTimeout(buttonEnable,3000);
 }
 
 
 function searchResult(data) {
 	let recipes = data.results;  //<--The array of recipe objects//
+	$('.card-row').empty();
+	// $offsetNumber += 9;
 	// console.log(data.results);
-	for (var i = 0; i < 9; i++) {
+
+	for (var i = 0; i < data.results.length; i++) {
 		let recipeId = recipes[i].id;
 		// console.log(recipes[i].id, $imageUrl + recipes[i].image);
 		$('.card-row').append(
 			`<div id="${recipeId}" class="card recipe-card medium col s12 m6 l4">
 				<div class="card-image waves-effect waves-block waves-light">
-					<img class="activator" src="${$imageUrl + recipes[i].image}">
+					<img class="activator image-tag" src="${$imageUrl + recipes[i].image}">
 				</div>
 				<div class="card-content">
 					<span class="card-title activator grey-text text-darken-4">${recipes[i].title}<i class="material-icons right">more_vert</i></span>
@@ -64,32 +88,54 @@ function searchResult(data) {
 					<span class="card-title grey-text text-darken-4">${recipes[i].title}<i class="material-icons right">close</i></span>
 					<ul class="ingredients-${recipeId}"></ul>
 					<h1 class="card-title">Instructions:</h1>
-					<div class="instructions-${recipeId}"></div>
+					<div class="instructions-${recipeId}">
+						<div class="preloader-wrapper active">
+							<div class="spinner-layer spinner-red-only">
+								<div class="circle-clipper left">
+									<div class="circle"></div>
+								</div><div class="gap-patch">
+									<div class="circle"></div>
+								</div><div class="circle-clipper right">
+									<div class="circle"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>`
 		);
 		// ingredientSearch(recipes[i].id);
 		// console.log(data.results);
 	}
+
 	$('.recipe-card').click(populateReveal);
 
 	$('.card-row').append(`<ul class="pagination">
-				<li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-				<li class="active"><a href="#!">1</a></li>
-				<li class="waves-effect"><a href="#!">2</a></li>
-				<li class="waves-effect"><a href="#!">3</a></li>
-				<li class="waves-effect"><a href="#!">4</a></li>
-				<li class="waves-effect"><a href="#!">5</a></li>
-				<li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+				<li class="waves-effect"><a class="more-arrows" href="#!"><i class="material-icons left-arrow">chevron_left</i></a></li>
+
+				<li class="waves-effect"><a class="more-arrows" href="#!"><i class="material-icons right-arrow">chevron_right</i></a></li>
 			</ul>`);
+
+	$('.right-arrow').click(function() {
+		page += 1;
+		recipeSearch();
+	});
+
+	$('.left-arrow').click(function() {
+		page += -1;
+		recipeSearch();
+	});
 }
+
 
 function populateReveal() {
 	let recipeId = $(this).attr('id');
 	let $instructions = $(`.instructions-${recipeId}`);
-	if (!$instructions.html()) {
+	if (!$instructions.hasClass('loaded')) {
 		// console.log('Populated');
 		ingredientSearch(recipeId);
+		// showIcon();
 	}
 }
 
@@ -99,19 +145,21 @@ function populateReveal() {
 
 //FUNCTION TO SEARCH RECIPES---WITH HEADER KEY CODE//
 
-function recipeSearch(search){
+function recipeSearch(){
 	$.ajax({
 		url: $searchUrl, // The URL to the API. You can get this in the API page of the API you intend to consume
 		// url:'recipes.json',
 		type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
 		data: {
-			'query': search,
+			'query': currentSearch, // page * number = offset
+			'number': perPage,
+			'offset': perPage * page,
 		}, // Additional parameters here
 		dataType: 'json',
 		success: searchResult,  //<--Callback Function to the API request for
 		error: function(err) { alert(err); },
 		beforeSend: function(xhr) {
-			xhr.setRequestHeader('X-Mashape-Authorization', 'AWSQqeJH4Umshj76kKpNlqShPjHxp17k89BjsnfcCDrDMru0b9'); // Enter here your Mashape key
+			// xhr.setRequestHeader('X-Mashape-Authorization', 'AWSQqeJH4Umshj76kKpNlqShPjHxp17k89BjsnfcCDrDMru0b9'); // Enter here your Mashape key
 		}
 	});
 }
@@ -129,13 +177,13 @@ function ingredientSearch(recipeId){
 		success: ingredientResult,
 		error: function(err) { alert(err); },
 		beforeSend: function(xhr) {
-			xhr.setRequestHeader('X-Mashape-Authorization', 'AWSQqeJH4Umshj76kKpNlqShPjHxp17k89BjsnfcCDrDMru0b9'); // Enter here your Mashape key
+			// xhr.setRequestHeader('X-Mashape-Authorization', 'AWSQqeJH4Umshj76kKpNlqShPjHxp17k89BjsnfcCDrDMru0b9'); // Enter here your Mashape key
 		}
 	});
 }
 
 function ingredientResult(data){
-	console.log(data);
+	// console.log(data);
 	let recipeId = data.id;
 	// console.log(data.id);
 	// let recipeId = currentRecipeId;
@@ -146,17 +194,26 @@ function ingredientResult(data){
 		);
 	}
 	if (data.instructions) {
-		$(`.instructions-${recipeId}`).append(
+		$(`.instructions-${recipeId}`).html(
 				'<p>' + data.instructions + '</p>'
 		);
 	} else {
-		$(`.instructions-${recipeId}`).append(
+		$(`.instructions-${recipeId}`).html(
 				'<p>Instructions Unfortuantely Not Available.</p>'
 		);
 	}
-
+	$(`.instructions-${recipeId}`).addClass('loaded');
 }
-
+function buttonDisable(){
+	$('#search-button').attr('disabled','disabled');
+}
+function buttonEnable(){
+	$('#search-button').removeAttr('disabled');
+}
+// function showIcon(){
+// 	// console.log(this);
+// 	$('.card-reveal').append();
+// }
 
 // function procedureResult(data){
 // 	console.log(data.instructions);
